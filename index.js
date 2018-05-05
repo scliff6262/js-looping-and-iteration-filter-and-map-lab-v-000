396ab7991b67cbdf7adf2096f3e064c55888989f
Sean Clifford <sclifford6262@gmail.com>
2018-05-05 17:02:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here:
+function driversWithRevenueOver(objects, revenue){
+  return objects.filter(function(object){return object.revenue > revenue})
+}
+
+function driverNamesWithRevenueOver(objects, revenue){
+  driverObjects = objects.filter(function(object){return object.revenue > revenue})
+  return driverObjects.map(function(object){return object.name})
+}
